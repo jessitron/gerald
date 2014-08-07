276aaf70712acd4f8ea174fd4b6619d541514cac
Jessica Kerr <jessitron@gmail.com>
2014-08-07 11:51:46 -0500

    An excellent edge case

--- spec/custom_generators.rb
+++ spec/custom_generators.rb
@@ -3,16 +3,15 @@ require 'rantly'
 
 class CustomGenerators
   class << self
-    @valid_events = [:search, :click]
 
     def rantly
       Rantly.singleton
     end
-    def activity_record
-      Generator.new( ->(){ { event: rantly.choose(@valid_events) } } ).not_shrinkable
+    def activity_record(events=[:search, :click])
+      Generator.new( ->(){ { event: rantly.choose(*events) } } ).not_shrinkable
     end
     def activity_record_for(event)
-      Generator.new( ->() {{event: event}}).not_shrinkable
+      activity_record([event])
     end
   end
 end
