8f48742d08b414122f12236f68466450df353421
Jessica Kerr <jessitron@gmail.com>
2014-08-07 10:22:33 -0500

    refactor simple functions

--- lib/report.rb
+++ lib/report.rb
@@ -4,7 +4,17 @@ class Report
 
   @sum = ->(a,b) { a + b }
 
-  def self.summarize(input)
-    { search: input.map { |i| if (i[:event] == :search) then 1 else 0 end }.reduce(&@sum) }
+  class << self
+    def get(field)
+       ->(m) { m[field]}
+    end
+
+    def count_if_eq(value)
+       ->(test) { if (test == value) then 1 else 0 end }
+    end
+
+    def summarize(input)
+      { search: input.map(&get(:event)).map(&count_if_eq(:search)).reduce(&@sum) }
+    end
   end
 end
