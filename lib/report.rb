dec7551827a20707fcdea5c0047a273df9d58934
Jessica Kerr <jessitron@gmail.com>
2014-08-07 10:43:57 -0500

    Refactored to something more general -> test failure

--- lib/report.rb
+++ lib/report.rb
@@ -13,8 +13,14 @@ class Report
        ->(test) { if (test == value) then 1 else 0 end }
     end
 
+    def count_event(event, input)
+      input.map(&get(:event)).map(&count_if_eq(:search)).reduce(&@sum)
+    end
+
     def summarize(input)
-      { search: input.map(&get(:event)).map(&count_if_eq(:search)).reduce(&@sum) }
+      unique_events = input.map(&get(:event)).uniq
+      puts unique_events
+      unique_events.inject({}){ |m, event| m.merge({event => count_event(event, input)}) }
     end
   end
 end
